32125b661fa410556f078f22862f8364083744c7
Haoyu <haoyu.wang141@outlook.com>
2024-03-03 11:14:47 +0800

    tmp

--- pages/example/example.js
+++ pages/example/example.js
@@ -4,5 +4,17 @@ Page({
   },
   handleTap: function() {
     console.log("触发回调函数")
+  },
+  goBilibili:function() {
+    const aid='200376800'
+    const timestamp=new Date().getTime()
+    const path=`pages/video/video?__preload_=${timestamp*10+3}&__key_=${timestamp*10+4}&avid=${aid}`
+    wx.navigateToMiniProgram({
+      appId: 'wx7564fd5313d24844',
+      path,
+      success: res => {
+        console.log('跳转成功')
+      }
+    })
   }
 })
