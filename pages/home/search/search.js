93569a8ce31f16616e77c88306e2259ea32047e4
YiyuLiu-Rin <rinland@qq.com>
2024-03-10 22:01:58 +0800

    [update] /home/search, /pub/matches

--- pages/home/search/search.js
+++ pages/home/search/search.js
@@ -1,24 +1,26 @@
 // pages/home/search/search.js
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-      userId: 0,
-      searchText: '',
-      newsIdList: [],
-      matchIdList: [],
-      playerIdList: [],
-      teamIdList: [],
-      eventIdList: [],
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userId: 0,
+    searchText: '',
+    newsIdList: [],
+    matchIdList: [],
+    playerIdList: [],
+    teamIdList: [],
+    eventIdList: [],
 
-      filterButtonText: '+',
-      showFilter: false,
-      filterTypes: ['全部', '新闻', '比赛', '球员(裁判)', '球队', '赛事', '其它'],
-      type: '全部',
-      filterRanks: ['按时间', '按热度'],
-      rank: '按时间',
+    filterButtonText: '+', // 弃用
+    showFilter: false,
+    filterTypes: ['全部', '新闻', '比赛', '球员(裁判)', '球队', '赛事', '其它'],
+    type: '全部',
+    filterSortings: ['按时间', '按热度'],
+    sorting: '按时间',
+    filterFavors: ['仅我的关注', '所有'],
+    favor: '仅我的关注'
 
     //   checkboxItems: [
     //     { name: '新闻', value: 'news', checked: true },
@@ -33,136 +35,143 @@ Page({
     //     { name: '按热度', value: 'popularity', checked: true },
     //   ]
 
-    },
-  
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad(options) {
-        this.setData({
-            searchText: decodeURIComponent(options.searchText)
-        });
-    },
-  
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
-  
-    },
-  
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
-  
-    },
-  
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
-  
-    },
-  
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
-  
-    },
-  
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
-  
-    },
-  
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
-  
-    },
-  
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
-  
-    },
-  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    this.setData({
+      searchText: decodeURIComponent(options.searchText)
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  },
+
   ///////////////////////////////////////////////////////////////////////////////
   // 其它逻辑
-  
-    /**
-     * 监听搜索框文本
-     */
-    bindInput: function(e) {
-      this.setData({
-        searchText: e.detail.value // 更新data中的searchText值为用户输入的内容
-      });
-      // 这里可以添加你的搜索逻辑，比如根据用户输入的内容进行实时搜索
-    },
-  
-    /**
-     * 监听搜索按钮
-     */
-    search: function() {
-      console.log('搜索内容:', this.data.searchText);
-    },
 
-    /**
-     * 监听过滤器按钮
-     */
-    filter: function() {
-      console.log('点击过滤器按钮');
-      this.setData({
-          filterButtonText: this.data.filterButtonText == '+' ? '-' : '+',
-          showFilter: !this.data.showFilter
-      });
-    },
+  /**
+   * 监听搜索框文本
+   */
+  bindInput: function (e) {
+    this.setData({
+      searchText: e.detail.value // 更新data中的searchText值为用户输入的内容
+    });
+    // 这里可以添加你的搜索逻辑，比如根据用户输入的内容进行实时搜索
+  },
 
-    bindTypeChange: function (e) {
-        const val = e.detail.value;
-        this.setData({
-            type: this.data.filterTypes[val]
-        });
-    },
+  /**
+   * 监听搜索按钮
+   */
+  search: function () {
+    console.log('搜索内容:', this.data.searchText);
+  },
 
-    bindRankChange: function (e) {
-        const val = e.detail.value;
-        this.setData({
-            rank: this.data.filterRanks[val]
-        });
-    },
+  /**
+   * 监听过滤器按钮
+   */
+  filter: function () {
+    console.log('点击过滤器按钮');
+    this.setData({
+      filterButtonText: this.data.filterButtonText == '+' ? '-' : '+',
+      showFilter: !this.data.showFilter
+    });
+  },
 
-    // showMultiPicker() {
-    //     wx.showActionSheet({
-    //       itemList: this.data.items,
-    //       success: res => {
-    //         // 用户选择的选项的索引数组
-    //         const selectedIndexes = res.tapIndex;
-    //         // 根据索引数组获取选中的选项，并保存到 selectedOptions 中
-    //         const selectedOptions = selectedIndexes.map(index => this.data.items[index]);
-    //         this.setData({ selectedOptions });
-    //       }
-    //     });
-    //   },
+  bindTypeChange: function (e) {
+    const val = e.detail.value;
+    this.setData({
+      type: this.data.filterTypes[val]
+    });
+  },
 
-    // checkboxChange(e) {
-    //     const checkedValues = e.detail.value; // 获取被选中的复选框的值
-    //     const items = this.data.items.map(item => {
-    //       // 遍历所有选项，更新选中状态
-    //       if (checkedValues.indexOf(item.value) !== -1) {
-    //         item.checked = true;
-    //       } else {
-    //         item.checked = false;
-    //       }
-    //       return item;
-    //     });
-    //     this.setData({ items });
-    // }
-  
-  })
+  bindSortingChange: function (e) {
+    const val = e.detail.value;
+    this.setData({
+      sorting: this.data.filterSortings[val]
+    });
+  },
+
+  bindFavorChange: function (e) {
+    const val = e.detail.value;
+    this.setData({
+      favor: this.data.filterFavors[val]
+    });
+  },
+
+  // showMultiPicker() {
+  //     wx.showActionSheet({
+  //       itemList: this.data.items,
+  //       success: res => {
+  //         // 用户选择的选项的索引数组
+  //         const selectedIndexes = res.tapIndex;
+  //         // 根据索引数组获取选中的选项，并保存到 selectedOptions 中
+  //         const selectedOptions = selectedIndexes.map(index => this.data.items[index]);
+  //         this.setData({ selectedOptions });
+  //       }
+  //     });
+  //   },
+
+  // checkboxChange(e) {
+  //     const checkedValues = e.detail.value; // 获取被选中的复选框的值
+  //     const items = this.data.items.map(item => {
+  //       // 遍历所有选项，更新选中状态
+  //       if (checkedValues.indexOf(item.value) !== -1) {
+  //         item.checked = true;
+  //       } else {
+  //         item.checked = false;
+  //       }
+  //       return item;
+  //     });
+  //     this.setData({ items });
+  // }
+
+})
--- pages/pub/matches/matches.js
+++ pages/pub/matches/matches.js
@@ -1,18 +1,31 @@
 // pages/pub/matches/matches.js
+const appInstance = getApp()
+const URL = appInstance.globalData.URL
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    searchText: '' // 初始化搜索框内容为空
+    // 搜索框
+    searchText: '',
+    // 过滤器
+    showFilter: false,
+    filterSortings: ['按时间', '按热度'],
+    sorting: '按时间',
+    filterFavors: ['仅我的关注', '所有'],
+    favor: '仅我的关注',
+    // 比赛列表
+    allMatches: [],
+    favorMatches: [],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    this.fetchData(options.id);
   },
 
   /**
@@ -68,7 +81,7 @@ Page({
   /**
    * 监听搜索框文本
    */
-  bindInput: function(e) {
+  bindInput: function (e) {
     this.setData({
       searchText: e.detail.value // 更新data中的searchText值为用户输入的内容
     });
@@ -78,9 +91,89 @@ Page({
   /**
    * 监听搜索按钮
    */
-  search: function() {
+  search: function () {
     // 这里添加搜索逻辑，比如发起网络请求或其他操作
     console.log('搜索内容:', this.data.searchText);
-  }
+  },
+
+  ////////////////////////////////////////////////////////////////
+  // HTTP 请求
+
+  fetchData: function (id) {
+
+    // 显示加载提示框，提示用户正在加载
+    wx.showLoading({
+      title: '加载中',
+      mask: true // 创建一个蒙层，防止用户操作
+    });
+
+    // 网络请求
+    wx.request({
+      url: URL + '/match/getAll',
+      success(res) {
+        console.log("/match/getAll ->")
+        console.log(res.data)
+        if (res.statusCode !== 200) {
+          console.log("请求失败，状态码为：" + res.statusCode + "; 错误信息为：" + res.data)
+          return
+        }
+        for (let match of res.data) {
+          allMatches.push(match)
+        }
+      },
+      fail(err) {
+        console.log('请求失败', err);
+        // 可以显示失败的提示信息，或者做一些错误处理
+      },
+      complete() {
+        // 无论请求成功还是失败都会执行
+        wx.hideLoading(); // 关闭加载提示框
+      }
+    });
+  },
+
+  ////////////////////////////////////////////////////////////////
+  // 监听
+
+  /**
+   * 监听过滤器按钮
+   */
+  filter: function () {
+    console.log('点击过滤器按钮');
+    this.setData({
+      filterButtonText: this.data.filterButtonText == '+' ? '-' : '+',
+      showFilter: !this.data.showFilter
+    });
+  },
+
+  /**
+   * 监听过滤器排序选项
+   */
+  bindSortingChange: function (e) {
+    const val = e.detail.value;
+    this.setData({
+      sorting: this.data.filterSortings[val]
+    });
+  },
+
+  /**
+   * 监听过滤器过滤选项
+   */
+  bindFavorChange: function (e) {
+    const val = e.detail.value;
+    this.setData({
+      favor: this.data.filterFavors[val]
+    });
+  },
+
+  /////////////////////////////////////////////////////
+  // 跳转
+
+  gotoMatch: function (e) {
+    const id = e.currentTarget.dataset.id
+    wx.navigateTo({
+      url: '/pages/pub/match/match?id=' + id,
+    })
+  },
 
 })
