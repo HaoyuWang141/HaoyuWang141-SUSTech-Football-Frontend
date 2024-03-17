7fac2fc16dd743670fbaae4c52208f105bc6505c
Haoyu <haoyu.wang141@outlook.com>
2024-03-17 22:11:28 +0800

    [update] home page

--- app.js
+++ app.js
@@ -1,49 +1,5 @@
 // app.js
 App({
-  onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        console.log(res)
-        if (res.code) {
-          //发起网络请求
-          wx.request({
-            url: this.globalData.SERVER + '/user/wxLogin?code=' + res.code,
-            method: "POST",
-            success(res) {
-              console.log(res.data)
-              const appInstance = getApp()
-              appInstance.globalData.openid = res.data.openid
-              appInstance.globalData.session_key = res.data.session_key
-              wx.request({
-                url: appInstance.globalData.URL + '/user/login?openid=' + res.data.openid + '&session_key=' + res.data.session_key,
-                method: "POST",
-                success(res) {
-                  console.log(res.data)
-                  appInstance.globalData.userId = res.data.userId
-                  appInstance.globalData.nickName = res.data.nickName
-                },
-                fail(err) {
-                  console.log(err)
-                }
-              })
-            }
-          })
-        } else {
-          console.log('登录失败！' + res.errMsg)
-        }
-      }
-    })
-
-    // 获取用户头像url
-    this.globalData.avatarUrl = wx.getStorageSync('avatarUrl')
-  },
   globalData: {
     URL: 'https://haoyu-wang141.top:8085',
     SERVER: 'https://haoyu-wang141.top:8085',
@@ -52,6 +8,94 @@ App({
     session_key: null,
     userId: null,
     nickName: null,
-    avatarUrl: null
-  }
+    avatarUrl: null,
+    requestQueue: [],
+  },
+
+  onLaunch() {
+    // 展示本地存储能力
+    const logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    this.userLogin()
+
+    // 获取用户头像url
+    this.globalData.avatarUrl = wx.getStorageSync('avatarUrl')
+  },
+
+  userLogin() {
+    var that = this
+    // 登录，获取code
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        console.log(res)
+        if (res.code) {
+          // 通过code获取openid和session_key
+          that.fetchOpenIdAndSessionKey(res.code)
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    })
+  },
+
+  fetchOpenIdAndSessionKey(code) {
+    // 显示加载提示框，提示用户正在加载
+    wx.showLoading({
+      title: '加载中',
+      mask: true // 创建一个蒙层，防止用户操作
+    });
+    var that = this
+    wx.request({
+      url: that.globalData.SERVER + '/user/wxLogin?code=' + code,
+      method: "POST",
+      success(res) {
+        console.log(res.data)
+        that.globalData.openid = res.data.openid
+        that.globalData.session_key = res.data.session_key
+        that.fetchUserId(res.data.openid, res.data.session_key)
+      },
+      complete() {
+        // 关闭加载提示框
+        wx.hideLoading();
+      }
+    })
+  },
+
+  fetchUserId(openid, session_key) {
+    var that = this
+    // 通过openid和session_key获取userId
+    wx.request({
+      url: that.globalData.URL + '/user/login?openid=' + openid + '&session_key=' + session_key,
+      method: "POST",
+      success(res) {
+        console.log(res.data)
+        that.globalData.userId = res.data.userId
+        that.globalData.nickName = res.data.nickName
+
+        that.processRequestQueue()
+      },
+      fail(err) {
+        console.log(err)
+      }
+    })
+  },
+
+  addToRequestQueue: function (task) {
+    if (this.globalData.userId) {
+      task(this.globalData.userId); // 如果userId已经获取，直接执行任务
+    } else {
+      this.globalData.requestQueue.push(task); // 否则加入队列
+    }
+  },
+
+  processRequestQueue: function () {
+    while (this.globalData.requestQueue.length > 0) {
+      const task = this.globalData.requestQueue.shift();
+      task(this.globalData.userId);
+    }
+  },
 })
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,24 +1,32 @@
 // pages/home/home.js
+const app = getApp()
+const URL = app.globalData.URL
+const {
+  formatTime
+} = require("../../utils/timeFormatter")
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    userId: 0,
     searchText: '', // 初始化搜索框内容为空
-    newsIdList: [],
-    matchIdList: [],
-    playerIdList: [],
-    teamIdList: [],
-    eventIdList: [],
+    newsList: [],
+    matchList: [],
+    playerList: [],
+    teamList: [],
+    eventList: [],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    app.addToRequestQueue(this.fetchFavorateMatch)
+    app.addToRequestQueue(this.fetchFavoratePlayer)
+    app.addToRequestQueue(this.fetchFavorateTeam)
+    app.addToRequestQueue(this.fetchFavorateEvent)
   },
 
   /**
@@ -70,9 +78,91 @@ Page({
 
   },
 
-///////////////////////////////////////////////////////////////////////////////
-// 页面跳转
+  /////////////////////////////////////
+  // 网络传输
+  fetchFavorateMatch(id) {
+    let that = this
+    wx.request({
+      url: URL + '/getFavorite',
+      data: {
+        userId: id,
+        type: 'match',
+      },
+      success(res) {
+        console.log("home page: fetchFavorite match ->")
+        console.log(res.data)
+        let matchList = res.data ?? []
+        for (let match of matchList) {
+          let date = new Date(match.time)
+          match.strTime = formatTime(date)
+          match.hasBegun = new Date() > date
+        }
+        that.setData({
+          matchList: matchList,
+        })
+      },
+    })
+  },
 
+  fetchFavoratePlayer(id) {
+    let that = this
+    wx.request({
+      url: URL + '/getFavorite',
+      data: {
+        userId: id,
+        type: 'player',
+      },
+      success(res) {
+        console.log("home page: fetchFavorite: players ->")
+        console.log(res.data)
+        let playerList = res.data ?? []
+        that.setData({
+          playerList: playerList,
+        })
+      },
+    })
+  },
+
+  fetchFavorateTeam(id) {
+    let that = this
+    wx.request({
+      url: URL + '/getFavorite',
+      data: {
+        userId: id,
+        type: 'team',
+      },
+      success(res) {
+        console.log("home page: fetchFavorite: teams ->")
+        console.log(res.data)
+        let teamList = res.data ?? []
+        that.setData({
+          teamList: teamList,
+        })
+      },
+    })
+  },
+
+  fetchFavorateEvent(id) {
+    let that = this
+    wx.request({
+      url: URL + '/getFavorite',
+      data: {
+        userId: id,
+        type: 'event',
+      },
+      success(res) {
+        console.log("home page: fetchFavorite: events ->")
+        console.log(res.data)
+        let eventList = res.data ?? []
+        that.setData({
+          eventList: eventList,
+        })
+      },
+    })
+  },
+
+  ///////////////////////////////////////////////////////////////////////////////
+  // 页面跳转
   gotoExample() {
     wx.navigateTo({
       url: '/pages/example/example',
@@ -85,31 +175,55 @@ Page({
     })
   },
 
-  gotoMoreMatches() {
+  gotoMatchesPage(e) {
     wx.navigateTo({
       url: '/pages/pub/matches/matches',
     })
   },
 
-  gotoMorePlayers() {
+  gotoPlayersPage(e) {
     wx.navigateTo({
-        url: '/pages/pub/players/players',
-      })
+      url: '/pages/pub/players/players',
+    })
   },
 
-  gotoMoreTeams() {
+  gotoTeamsPage(e) {
     wx.navigateTo({
-        url: '/pages/pub/teams/teams',
-      })
+      url: '/pages/pub/teams/teams',
+    })
   },
 
-///////////////////////////////////////////////////////////////////////////////
-// 其它逻辑
+  // 跳转至event详情页，需要的参数为event的id 
+  gotoEvent: function (e) {
+    const dataset = e.currentTarget.dataset
+    wx.navigateTo({
+      url: '/pages/pub/event/event?id=' + dataset.id,
+    })
+  },
+
+  // 跳转至news详情页，需要的参数为news的id
+  gotoNews: function (e) {
+    const dataset = e.currentTarget.dataset
+    wx.navigateTo({
+      url: '/pages/pub/news/news?id=' + dataset.id,
+    })
+  },
+
+  // 跳转至match详情页，需要的参数为match的id
+  gotoMatch: function (e) {
+    const dataset = e.currentTarget.dataset
+    wx.navigateTo({
+      url: '/pages/pub/match/match?id=' + dataset.id,
+    })
+  },
+
+  ///////////////////////////////////////////////////////////////////////////////
+  // 其它逻辑
 
   /**
    * 监听搜索框文本
    */
-  bindInput: function(e) {
+  bindInput: function (e) {
     this.setData({
       searchText: e.detail.value // 更新data中的searchText值为用户输入的内容
     });
@@ -119,34 +233,10 @@ Page({
   /**
    * 监听搜索按钮
    */
-  search: function() {
+  search: function () {
     // 这里添加搜索逻辑，比如发起网络请求或其他操作
     console.log('搜索内容:', this.data.searchText);
     this.gotoSearch();
   },
-
-  // 跳转至event详情页，需要的参数为event的id 
-  gotoEvent: function(e) {
-    const dataset = e.currentTarget.dataset
-    wx.navigateTo({
-      url: '/pages/pub/event/event?id=' + dataset.id,
-    })
-  },
-
-  // 跳转至news详情页，需要的参数为news的id
-  gotoNews: function(e) {
-    const dataset = e.currentTarget.dataset
-    wx.navigateTo({
-      url: '/pages/pub/news/news?id=' + dataset.id,
-    })
-  },
-
-  // 跳转至match详情页，需要的参数为match的id
-  gotoMatch: function(e) {
-    const dataset = e.currentTarget.dataset
-    wx.navigateTo({
-      url: '/pages/pub/match/match?id=' + dataset.id,
-    })
-  }
 
 })
