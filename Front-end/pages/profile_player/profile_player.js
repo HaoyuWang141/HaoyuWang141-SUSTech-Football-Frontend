a209b9b615be7f24cfa290e31e1959001cb7227d
Haoyu <haoyu.wang141@outlook.com>
2024-03-22 14:11:41 +0800

    [update] profile player

--- Front-end/pages/profile_player/profile_player.js
+++ Front-end/pages/profile_player/profile_player.js
@@ -1,18 +1,25 @@
-// pages/profile_player/profile_player.js
+const app = getApp()
+const URL = app.globalData.URL
+const {
+  formatTime
+} = require("../../utils/timeFormatter")
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    matchList: [],
+    teamList: [],
+    eventList: [],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    app.addToRequestQueue(this.fetchPlayerId)
   },
 
   /**
@@ -47,7 +54,7 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh() {
-
+    app.addToRequestQueue(this.fetchPlayerId)
   },
 
   /**
@@ -64,8 +71,114 @@ Page({
 
   },
 
-///////////////////////////////////////////////////////////////////////////////
-// 页面跳转
+  // 拉取数据
+  fetchPlayerId(userId) {
+    let that = this
+    wx.request({
+      url: URL + '/user/getPlayerId',
+      data: {
+        userId: userId,
+      },
+      success(res) {
+        console.log("profile player page: fetchPlayerId ->")
+        if (res.statusCode != 200) {
+          console.error("请求失败，状态码为：" + res.statusCode + "; 错误信息为：" + res.data)
+          return
+        }
+        console.log(res.data)
+        let playerId = res.data
+        that.fetchPlayerMatches(playerId)
+        that.fetchPlayerTeams(playerId)
+        that.fetchPlayerEvents(playerId)
+      },
+      fail(err) {
+        console.error('请求失败：', err.statusCode, err.errMsg);
+      },
+    })
+  },
+
+  fetchPlayerMatches(playerId) {
+    let that = this
+    wx.request({
+      url: URL + '/player/match/getAll',
+      data: {
+        playerId: playerId,
+      },
+      success(res) {
+        console.log("profile player page: fetchPlayerMatches ->")
+        if (res.statusCode != 200) {
+          console.error("请求失败，状态码为：" + res.statusCode + "; 错误信息为：" + res.data)
+          return
+        }
+        console.log(res.data)
+        let matchList = res.data ?? []
+        for (let match of matchList) {
+          let date = new Date(match.time)
+          match.strTime = formatTime(date)
+          match.hasBegun = new Date() > date
+        }
+        that.setData({
+          matchList: matchList,
+        })
+      },
+      fail(err) {
+        console.error('请求失败：', err.statusCode, err.errMsg);
+      },
+    })
+  },
+
+  fetchPlayerTeams(playerId) {
+    let that = this
+    wx.request({
+      url: URL + '/player/team/getAll',
+      data: {
+        playerId: playerId,
+      },
+      success(res) {
+        console.log("profile player page: fetchPlayerTeams ->")
+        if (res.statusCode != 200) {
+          console.error("请求失败，状态码为：" + res.statusCode + "; 错误信息为：" + res.data)
+          return
+        }
+        console.log(res.data)
+        let teamList = res.data ?? []
+        that.setData({
+          teamList: teamList,
+        })
+      },
+      fail(err) {
+        console.error('请求失败：', err.statusCode, err.errMsg);
+      },
+    })
+  },
+
+  fetchPlayerEvents(playerId) {
+    let that = this
+    wx.request({
+      url: URL + '/player/event/getAll',
+      data: {
+        playerId: playerId,
+      },
+      success(res) {
+        console.log("profile player page: fetchPlayerEvents ->")
+        if (res.statusCode != 200) {
+          console.error("请求失败，状态码为：" + res.statusCode + "; 错误信息为：" + res.data)
+          return
+        }
+        console.log(res.data)
+        let eventList = res.data ?? []
+        that.setData({
+          eventList: eventList,
+        })
+      },
+      fail(err) {
+        console.error('请求失败：', err.statusCode, err.errMsg);
+      },
+    })
+  },
+
+  ///////////////////////////////////////////////////////////////////////////////
+  // 页面跳转
   edit_information() {
     wx.navigateTo({
       url: '/pages/profile_player/profile_player_edit/profile_player_edit',
@@ -84,33 +197,40 @@ Page({
     })
   },
 
-  gotoTeams() {
+  gotoMatchesPage(e) {
+    let matchList = e.currentTarget.dataset.list ?? []
+    let matchIdList = matchList.map(match => match.matchId)
     wx.navigateTo({
-      url: '/pages/pub/teams/teams',
+      url: '/pages/pub/matches/matches?idList=' + matchIdList,
     })
   },
 
-  gotoTeam() {
+  gotoMatchPage: function (e) {
+    const dataset = e.currentTarget.dataset
     wx.navigateTo({
-      url: '/pages/pub/team/team',
+      url: '/pages/pub/match/match?id=' + dataset.id,
     })
   },
 
-  gotoMatches() {
+  gotoTeamsPage(e) {
+    let teamList = e.currentTarget.dataset.list ?? []
+    let teamIdList = teamList.map(team => team.teamId)
     wx.navigateTo({
-      url: '/pages/pub/matches/matches',
+      url: '/pages/pub/teams/teams?idList=' + teamIdList,
     })
   },
 
-  gotoMatch() {
+  gotoTeamPage: function (e) {
+    let id = e.currentTarget.dataset.id
     wx.navigateTo({
-      url: '/pages/pub/match/match',
+      url: '/pages/pub/team/team?id=' + id,
     })
   },
 
-  gotoEvent() {
+  gotoEventPage: function (e) {
+    const dataset = e.currentTarget.dataset
     wx.navigateTo({
-      url: '/pages/pub/event/event',
+      url: '/pages/pub/event/event?id=' + dataset.id,
     })
   },
 
