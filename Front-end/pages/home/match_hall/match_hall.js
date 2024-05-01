9ca23baeb6621f5c9cffb46e3d5922ac4a88a1f7
YiyuLiu-Rin <rinland@qq.com>
2024-05-01 18:19:19 +0800

    [fix] searchFilter, [update] home/halls(完成)

--- Front-end/pages/home/match_hall/match_hall.js
+++ Front-end/pages/home/match_hall/match_hall.js
@@ -1,6 +1,12 @@
 // pages/home/event_hall/event_hall.js
 const app = getApp()
 const URL = app.globalData.URL
+const {
+  formatTime
+} = require("../../../utils/timeFormatter")
+const {
+  filter
+} = require("../../../utils/searchFilter")
 
 Page({
 
@@ -8,14 +14,15 @@ Page({
    * 页面的初始数据
    */
   data: {
-    eventList: []
+    eventList: [],
+    searchText: '',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-    this.fetchData()
+
   },
 
   /**
@@ -29,7 +36,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow() {
-
+    app.addToRequestQueue(this.fetchData)
   },
 
   /**
@@ -50,7 +57,7 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh() {
-
+    app.addToRequestQueue(this.fetchData)
   },
 
   /**
@@ -67,35 +74,68 @@ Page({
 
   },
 
-  fetchData() {
+  fetchData(id) {
+    this.setData({
+      matchList: [],
+    })
     let that = this
     wx.request({
-      url: URL + '/event/getAll',
+      url: URL + '/match/getAll',
       success(res) {
-        console.log("event hall -> fetchData:")
+        console.log("search page: fetch match ->")
         if (res.statusCode != 200) {
           console.error("请求失败，状态码为：" + res.statusCode + "; 错误信息为：" + res.data)
           return
         }
         console.log(res.data)
+        let matchList = res.data ?? []
+        let filterMatchList = []
+        for (let match of matchList) {
+          let patterns = [(match.homeTeam ? match.homeTeam.name : ''), (match.awayTeam ? match.awayTeam.name : ''), (match.matchEvent ? match.matchEvent.eventName : '')]
+          if (filter(that.data.searchText,  patterns)) {
+            let date = new Date(match.time)
+            match.strTime = formatTime(date)
+            match.hasBegun = match.status == 'PENDING' ? false : true
+            filterMatchList.push(match)
+          }
+        }
         that.setData({
-          eventList: res.data
+          matchList: filterMatchList,
         })
       },
-      fail: function(err) {
+      fail: function (err) {
         console.error('请求失败：', err.statusCode, err.errMsg);
       },
     })
   },
 
-  gotoEventPage: function (e) {
+  // 监听
+
+  bindInput: function (e) {
+    this.setData({
+      searchText: e.detail.value,
+    });
+  },
+
+  search: function () {
+    console.log('搜索内容:', this.data.searchText);
+    app.addToRequestQueue(this.fetchData)
+  },
+
+  showAll: function () {
+    this.setData({
+      searchText: '',
+    });
+    app.addToRequestQueue(this.fetchData)
+  },
+
+  // 跳转
+
+  gotoMatchPage: function (e) {
     const dataset = e.currentTarget.dataset
     wx.navigateTo({
-      url: '/pages/pub/event/event?id=' + dataset.id,
+      url: '/pages/pub/match/match?id=' + dataset.id,
     })
   },
 
-  search: function (e) {
-
-  },
 })
